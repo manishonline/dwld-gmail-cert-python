543b42bd988af1372cbb2c08d83550c49f691e8c
Manish Kumar <mkumar@numerix.com>
2017-10-29 21:33:35 +1100

    Password analysis

---
+++ password_analysis.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+from pandas import Series,DataFrame
+import matplotlib.pyplot as plt
+from urllib.request import urlopen
+import seaborn as sns
+
+
+def run():
+    url = 'http://www.dazzlepod.com/site_media/txt/passwords.txt'
+    response = urlopen(url)
+    print(response.info())
+
+    file = open('passwords.txt','x')
+    file.truncate()
+
+    for line in response.read().decode('utf-8'):
+        file.write(line)
+    file.close()
+
+    fh = open('passwords.txt','r+')
+
+    for line in fh.readline()[:20]:
+        print(line)
+
+
+if __name__ == '__main__':
+    run()
