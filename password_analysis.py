8e496b083085104fa26e5d9e1d0fcbcd841e34fc
Manish Kumar <mkumar@numerix.com>
2017-10-30 16:16:11 +1100

    Password analysis

--- password_analysis.py
+++ password_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from pandas import Series,DataFrame
+from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 from urllib.request import urlopen
 import seaborn as sns
@@ -11,19 +11,20 @@ def run():
     response = urlopen(url)
     print(response.info())
 
-    file = open('passwords.txt','x')
+    file = open('passwords.txt', 'x')
     file.truncate()
 
     for line in response.read().decode('utf-8'):
         file.write(line)
     file.close()
 
-    fh = open('passwords.txt','r+')
+    fh = open('passwords.txt', 'r+')
 
     for line in fh.readline()[:20]:
         print(line)
 
-    #Test comment
+        # Test comment
+
 
 if __name__ == '__main__':
     run()
